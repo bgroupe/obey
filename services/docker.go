0d320eae5c3ab2216be284896e41574b9ec7abc9
Casey <casey@peek.com>
2021-02-15 17:04:25 -0800

    container fetching

---
+++ services/docker.go
@@ -0,0 +1,119 @@
+package services
+
+// https://pkg.go.dev/github.com/docker/docker
+import (
+	"context"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+const (
+	scrapeLabel  = "obey.com/scrape"
+	versionLabel = "obey.com/version"
+	serviceLabel = "obey.com/serviceName"
+)
+
+// func main() {
+// 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	dc := DockerClient{
+// 		Client: cli,
+// 		Config: DockerClientConfig{
+// 			scrapeAll:                false,
+// 			deriveMetadataFromLabels: true,
+// 		},
+// 	}
+
+// 	scraped, err := dc.Scrape()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	for _, sc := range scraped {
+// 		fmt.Println("scraped:", sc.Name, sc.Version, sc.ImageSha, sc.State, sc.Created, sc.Status)
+// 	}
+// }
+
+// Scrape scrapes all containers based on configured labels
+func (d *DockerClient) Scrape() ([]ScrapedContainer, error) {
+	ctx := context.Background()
+	containers, err := d.Client.ContainerList(ctx, types.ContainerListOptions{})
+
+	if err != nil {
+		return []ScrapedContainer{}, err
+	}
+
+	// TODO: Export scraped containers to filter function for scrapeAll logic
+	var scSlice []ScrapedContainer
+	for _, container := range containers {
+		labels := container.Labels
+		if val, _ := labels[scrapeLabel]; val == "true" {
+			var sc ScrapedContainer
+			shortSha := strings.Split(container.ImageID, "sha256:")[1]
+			sc.Name = d.deriveServiceName(container)
+			sc.Version = d.deriveServiceVersion(container)
+			sc.State = container.State
+			sc.Status = container.Status
+			sc.ImageSha = shortSha[:7]
+			scSlice = append(scSlice, sc)
+		}
+	}
+
+	return scSlice, err
+}
+
+func (d *DockerClient) deriveServiceVersion(container types.Container) string {
+	if d.Config.deriveMetadataFromLabels {
+		if version, ok := container.Labels[versionLabel]; ok {
+			return version
+		}
+		return "Label `version` Not Found"
+	}
+	tag := strings.SplitAfter(container.Image, ":")[1]
+	return tag
+}
+
+func (d *DockerClient) deriveServiceName(container types.Container) string {
+	if d.Config.deriveMetadataFromLabels {
+		if name, ok := container.Labels[serviceLabel]; ok {
+			return name
+		}
+		return "Label `serviceName` Not Found"
+	}
+	// taking  the first name here
+	return container.Names[0]
+}
+
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+// NewDockerClient returns a new DockerClient
+func NewDockerClient() (*DockerClient, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, err
+	}
+
+	dc := DockerClient{
+		Client: cli,
+		Config: DockerClientConfig{
+			scrapeAll:                false,
+			deriveMetadataFromLabels: true,
+		},
+	}
+
+	return &dc, nil
+
+}
---
+++ services/types.go
@@ -0,0 +1,34 @@
+package services
+
+import (
+	"github.com/docker/docker/client"
+)
+
+// Client - Main interface for worker
+type Client interface {
+	Scrape()
+	Stat()
+	Logs()
+}
+
+// DockerClientConfig - config object for Docker client
+type DockerClientConfig struct {
+	scrapeAll                bool
+	deriveMetadataFromLabels bool
+}
+
+// DockerClient - struct for hold client config, implements Client interface
+type DockerClient struct {
+	Client *client.Client
+	Config DockerClientConfig
+}
+
+// ScrapedContainer used for serializing container data back to the worker
+type ScrapedContainer struct {
+	Name     string
+	Version  string
+	State    string
+	Status   string
+	Created  string
+	ImageSha string
+}
